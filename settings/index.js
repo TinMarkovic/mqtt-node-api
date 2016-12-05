82dc2766d76b5d6f9ad9ac0db26585cd08c3bf7a
Tin <tinmarkoviccs@gmail.com>
2016-12-05 15:57:53 +0100

    Fixing discrepancy between local and server

--- settings/index.js
+++ settings/index.js
@@ -4,14 +4,14 @@ const defaultConfig = require('./default.json');
 
 try {
   const localConfig = require('../../mqtt-node-api.settings.json');
+  config = merge(defaultConfig, localConfig);
 } catch (e) {
   if (e.code !== 'MODULE_NOT_FOUND') {
     console.log(`Module "local.js" failed to load. Error: ${e.message}`);
   } else {
     console.log(`Settings file not found, loading default settings.`);
+    config = localConfig;
   }
 }
-
-config = merge(defaultConfig, localConfig);
 
 module.exports = config;
