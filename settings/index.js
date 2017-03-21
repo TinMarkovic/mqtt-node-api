0729f1892a58111bee00b8c8593a59b4cc968908
Tin <tinmarkoviccs@gmail.com>
2017-03-21 17:40:11 +0100

    Added settings changes

--- settings/index.js
+++ settings/index.js
@@ -2,12 +2,16 @@ const merge = require('lodash/merge');
 
 const defaultConfig = require('./default.json');
 
+var pathToLocalConfig = process.env.TXOC_WEB_API_SETTINGS ? 
+                        process.env.TXOC_WEB_API_SETTINGS : 
+                        './txoc-web-api.settings.json';
+
 try {
-  const localConfig = require('../../mqtt-node-api.settings.json');
+  const localConfig = require(pathToLocalConfig);
   config = merge(defaultConfig, localConfig);
 } catch (e) {
   if (e.code !== 'MODULE_NOT_FOUND') {
-    console.log(`Module "local.js" failed to load. Error: ${e.message}`);
+    console.log(`Settings failed to load. Error: ${e.message}`);
   } else {
     console.log(`Settings file not found, loading default settings.`);
     config = defaultConfig;
